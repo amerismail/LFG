cd0d44dc239cdeb9fb1cdc3d14b647ef9d935506
Amer Ismail <aismailg07@gmail.com>
2015-07-23 15:42:40 -0500

    Api now Returns Json

--- LFG.Web/App_Start/WebApiConfig.cs
+++ LFG.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace LFG.Web
@@ -23,6 +24,7 @@ namespace LFG.Web
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
