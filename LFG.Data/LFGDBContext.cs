644cfd313cc61241696827113ce4ee988b5c76db
awfeese <feese83@gmail.com>
2015-07-23 13:01:23 -0500

    adding source

---
+++ LFG.Data/LFGDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LFG.Model;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LFG.Data
+{
+    public class LfgDbContext : DbContext
+    {
+        public LfgDbContext()
+            : base(nameOrConnectionString: "DefaultConnection") { }
+
+        public DbSet<Activity> Activities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
