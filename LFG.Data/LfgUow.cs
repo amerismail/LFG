deb74e76e826ae9c1c3e0398a04e2015624bb232
awfeese <feese83@gmail.com>
2015-07-23 14:26:43 -0500

    adding unit of work

---
+++ LFG.Data/LfgUow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LFG.Model;
+
+namespace LFG.Data
+{
+    class LfgUow : IDisposable
+    {
+        private LfgDbContext DbContext { get; set; }
+        
+        public LfgUow()
+        {
+            CreateDbContext();
+        }
+
+        public IRepository<Activity> Activities { get { return new EFRepository<Activity>(DbContext); } }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+
+        protected void CreateDbContext()
+        {
+            DbContext = new LfgDbContext();
+            DbContext.Configuration.ProxyCreationEnabled = false;
+            DbContext.Configuration.LazyLoadingEnabled = false;
+            DbContext.Configuration.ValidateOnSaveEnabled = false;
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (DbContext != null)
+                {
+                    DbContext.Dispose();
+                }
+            }
+        }
+        #endregion
+    }
+}
