72beec258162ee389febd50b315da03549894353
Amer Ismail <aismailg07@gmail.com>
2015-07-27 16:40:20 -0500

    Databinding set and can post a new activity

--- LFG.Web/Scripts/app/vm.activity.js
+++ LFG.Web/Scripts/app/vm.activity.js
@@ -1,5 +1,24 @@
 ﻿var ViewModel = function () {
-    this.name = ko.observable("Test");
+    this.name = ko.observable();
+    this.date = ko.observable();
+
+    this.save = function(){
+        save(ko.toJSON(this));
+    }
 };
 
 ko.applyBindings(new ViewModel());
+
+var save = function(vm) {
+    console.log(vm);
+        $.ajax({
+        url: "api/activities",
+        contentType: 'application/json',
+        method: "POST",
+        data: vm,
+        dataType: "json",
+        success: function () {
+            console.log("It worked");
+        }
+    });
+};
