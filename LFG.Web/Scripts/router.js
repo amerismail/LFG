8422b55d7941a833c7f3d1b8f39abea5d1e4e071
Amer Ismail <aismailg07@gmail.com>
2015-07-28 15:43:51 -0500

    Added router and main index page

---
+++ LFG.Web/Scripts/router.js
@@ -0,0 +1,23 @@
+﻿var name = function($,sammy) {
+    console.log("name");
+    var init = function () {
+        app.run('#/');
+    }
+
+    console.log("name");
+    var app = sammy('#main', function () {
+
+
+        console.log("Its running");
+        this.get("#/", function (context) {
+            context.render('Home.cshtml', { 'myVariable': 'Some value' }, function (view) {
+                $('#main').html(view);
+            });
+
+        })
+
+    })
+    
+    init();
+} (jQuery, Sammy)
+
