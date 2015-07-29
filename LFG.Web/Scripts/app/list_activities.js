87d3d921d805aba50d90b30c668e39a7ffdb285d
Amer Ismail <aismailg07@gmail.com>
2015-07-29 15:00:36 -0500

    Edited homepage to use bootstrap, created list_activties.js

---
+++ LFG.Web/Scripts/app/list_activities.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: "api/activities",
+        contentType: 'application/json',
+        method: "GET",
+        dataType: "json",
+        success: function (listOfActivities) {
+            updateDom(listOfActivities);
+        }
+    });
+
+    var updateDom = function (listOfActivities) {
+        console.log(listOfActivities);
+        var jsonObj = $.parseJSON('[' + JSON.stringify(listOfActivities) + ']');
+
+        for(var i = 0; i < jsonObj[0].length; i++)
+        {
+            console.log(jsonObj[0][i].Name);
+            $('#Test tr:last').after('<tr class="info"><td>' + jsonObj[0][i].Name + '</td>' + '<td>' + jsonObj[0][i].Date + '</td></tr>');
+        }       
+    }
+});
