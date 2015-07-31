1de9fadeb11b5d1c156c9cc05497a00a9a4147ba
Amer Ismail <aismailg07@gmail.com>
2015-07-31 13:40:30 -0500

    Adding User Stories and adding search model

---
+++ LFG.Model/ActivitiesSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFG.Model
+{
+    public class ActivitiesSearch
+    {
+        public string Name { get; set; }
+        //public string Console { get; set; }
+        //public string Game { get; set; }
+        //public bool Microphone { get; set; }
+    }
+}
---
+++ LFG.Web/Scripts/app/vm.search.js
@@ -0,0 +1,25 @@
+﻿var ActivitySearchViewModel = {
+    name: ko.observable(),
+
+    search: function () {
+        search(ko.toJSON(this));
+    }
+};
+
+var search = function (data) {
+    console.log(data);
+    $.ajax({
+        url: "api/activities/Search",
+        contentType: 'application/json; charset=utf-8',
+        method: "POST",
+        data: data,
+        traditional: true,
+        dataType: "json",
+        success: function (listOfActivities) {
+            console.log(listOfActivities);
+        }
+    });
+};
+
+ko.applyBindings(ActivitySearchViewModel, document.getElementById("Search"));
+
