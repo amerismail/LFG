be1c695294a1a6893ecb41e1f470ac4559aa9905
awfeese <feese83@gmail.com>
2015-08-07 13:35:48 -0500

    scripts/css are now bundled

--- LFG.Web/App_Start/BundleConfig.cs
+++ LFG.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace LFG.Web
 {
@@ -8,36 +7,31 @@ namespace LFG.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/app.css", 
+                "~/Content/loader.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/index").Include(
+                "~/Scripts/lib/jquery-2.1.4.min.js",
+                "~/Scripts/lib/knockout-3.3.0.js",
+                "~/Scripts/lib/sammy-0.7.5.min.js",
+                "~/Scripts/router.js",
+                "~/Content/bootstrap-3.3.5/js/bootstrap.min.js",
+                "~/Scripts/app/navbar.js"
+                ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/home").Include(
+                "~/Scripts/lib/jquery.tablesorter.min.js",
+                "~/Scripts/app/tableSorter.js",
+                "~/Scripts/app/list_activities.js",
+                "~/Scripts/app/vm.search.js"
+                ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/createActivity").Include(
+                "~/Scripts/app/vm.activity.js"
+                ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+           BundleTable.EnableOptimizations = true;
         }
     }
 }
